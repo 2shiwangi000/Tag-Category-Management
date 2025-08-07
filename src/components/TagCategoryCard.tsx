f6604b66514ead8d1086e3a17d3febd57c4436f0
Shiwangi Singh <2shiwangi000@gmail.com>
2025-08-08 00:03:41 +0530

    tagcategory card added

---
+++ src/components/TagCategoryCard.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import type { ITagCategory } from '../interfaces/interfaces';
+import styles from '../styles/Card.module.scss';
+
+type Props = {
+  category: ITagCategory;
+  onEdit: (item: ITagCategory) => void;
+  onDelete: (id: string) => void;
+};
+
+const TagCategoryCard = ({ category, onEdit, onDelete }: Props) => {
+  return (
+    <div className={styles.card}>
+      <h3>{category.name}</h3>
+      <p>Status: {category.status}</p>
+      <p>Group: {category.group.label}</p>
+      <p>Precision: {category.precisionType}</p>
+      <button onClick={() => onEdit(category)}>Edit</button>
+      <button onClick={() => onDelete(category.id)}>Delete</button>
+    </div>
+  );
+}
+
+export default TagCategoryCard
